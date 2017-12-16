74fc0e48e3cde9ab3dc8006b33e0e71081a5adb9
ZINAHAL <albaghdz@tcd.ie>
2017-12-16 23:54:08 +0000

    added classes and methods but requires sorting out

--- SoftwareEngineering/github_API_data_retrieval/GetAPIData.java
+++ SoftwareEngineering/github_API_data_retrieval/GetAPIData.java
@@ -1,6 +1,8 @@
 import java.io.*;
 
+
 import java.net.*;
+import java.time.MonthDay;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -23,49 +25,81 @@ import com.google.gson.JsonParser;
  *
  */
 public class GetAPIData {
-
 	
-	public static void main(String[] args) {
+	
+	private String user_name;
+	static MonthDay date = MonthDay.of(11, 2);
+	
+	static private int n = date.getDayOfMonth();
+	static int sd = n;
+	
+	public static void main(String[] args) throws Exception {
 		
-		URL github_url;
-		HttpURLConnection request;
-		JsonParser json_parser;
+		//UsersPushData zinah = new UsersPushData("phadej", 1, MonthDay.of(12, 10), MonthDay.of(12, 14));
+		//zinah.getFollowersUsernames("phadej", 1);
 		
-		try
+		UsersPushData x = new UsersPushData("reeze", 2, MonthDay.of(12, 10), MonthDay.of(12, 14));
+		x.getFollowersUsernames("reeze", 1);
+		int i = 0;
+		Iterator it = x.followers_usernames.iterator();
+		while(it.hasNext())
 		{
-			
-			int page_number = 1;
-			while(page_number <= 10)
-			{
-				github_url = new URL("https://api.github.com/users/phadej/events?page=" + page_number++);
-				request = (HttpURLConnection) github_url.openConnection();
-			    request.connect();
-			    
-				json_parser = new JsonParser();
-				JsonElement parsed_github_url = json_parser.parse(new InputStreamReader((InputStream) request.getContent()));
-				JsonArray parsed_url_array = parsed_github_url.getAsJsonArray();
-				
-				for(JsonElement x: parsed_url_array)
-				{
-					JsonObject y = x.getAsJsonObject();
-					if(y.get("type").getAsString().equals("PushEvent")) {
-						String creation_date = y.get("created_at").getAsString();
-						int day = getDay(creation_date);
-						int month = getMonth(creation_date);
-						if(month == 12 && (day >= 10 && day <= 15))
-						{
-							System.out.println("success");
-						}
-					}
-				}
-				
-			}
-			
-			System.out.println("great it's working for now");
-			
+			i++;
+			System.out.println(it.next());
 		}
-		catch(IOException e) { e.printStackTrace(); }
-		catch(Exception e) { e.printStackTrace(); }
+		System.out.println(i);
+		System.out.println(x.followers_usernames.size());
+//		URL github_url;
+//		HttpURLConnection request;
+//		JsonParser json_parser;
+//		
+//		try
+//		{
+//			//String url_path = "https://api.github.com/users/phadej/events?page=";
+//			int page_number = 1;
+//			while(page_number <= 20)
+//			{
+//				github_url = new URL("https://api.github.com/users/phadej/events?page=" + page_number);
+//				request = (HttpURLConnection) github_url.openConnection();
+//				request.setRequestMethod("GET");
+//			    request.connect();
+//			    System.out.println(request.getResponseCode());
+//			    if(request.getResponseCode() != 200)
+//			    {
+//			    	break;
+//			    }
+//			    
+//				json_parser = new JsonParser();
+//				JsonElement parsed_github_url = json_parser.parse(new InputStreamReader((InputStream) request.getContent()));
+//				JsonArray parsed_url_array = parsed_github_url.getAsJsonArray();
+//				
+//				for(JsonElement element: parsed_url_array)
+//				{
+//					JsonObject x = element.getAsJsonObject();
+//					if(x.get("type").getAsString().equals("PushEvent")) 
+//					{
+//						String creation_date = x.get("created_at").getAsString();
+//						int day = getDay(creation_date);
+//						int month = getMonth(creation_date);
+//						if((month >= 12) && (month <= 12) && (day >= 10) && (day <= 15))
+//						{
+//							System.out.println(x.get("id").getAsString());
+//							System.out.println("z");
+//						}
+//					}
+//				}
+//				
+//				System.out.println(page_number);
+//				page_number++;
+//			}
+//			
+//			
+//			System.out.println("great it's working for now");
+//			//System.out.println(sd);
+//			
+//		}
+//		catch(IOException e) { e.printStackTrace(); }
+//		catch(Exception e) { e.printStackTrace(); }
 		
 	}
 	
@@ -93,6 +127,12 @@ public class GetAPIData {
 		} 
 		
 		return Integer.parseInt(str_num);
+	}
+	
+	
+	public static int getUserCommitsPerWeek(String user_name)
+	{
+		return 9;
 	}
 	
 
